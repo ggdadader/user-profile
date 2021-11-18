abea7485fc34510b817f6e9016cd42dfe7c23d7c
ggdadader <xider1091@gmail.com>
2021-11-18 02:27:13 -0500

    Initialize Project

---
+++ src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import NotFound from '../views/NotFound.vue'
+import User from '../views/User.vue'
+
+
+
+
+
+Vue.use(Router)
+
+
+export default new Router({
+  linkExactActiveClass: 'active',
+  routes: [
+    {
+      path: '/',
+      name: 'root',
+      redirect: '/User'
+    },
+    {
+      path: '/signin',
+      name: 'sign-in',
+      component: SignIn
+    },
+    {
+      path: '/signup',
+      name: 'sign-up',
+      component: () => import('../views/SignUp.vue')
+    },
+
+    {
+      path: '/user',
+      name: 'user',
+      component: User
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: () => import('../views/User.vue')
+    },
+    
+    {
+      path: '*',
+      name: 'not-found',
+      component: NotFound
+    },
+  ]
+})
